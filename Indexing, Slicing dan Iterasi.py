9b5de6d063f55053de658e4e59e48b697f731821
Noptzy <nopsy.zet@gmail.com>
2024-09-30 17:39:21 +0700

    update

---
+++ Indexing, Slicing dan Iterasi.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+a = np.arange(10)**2
+
+print(a)
+
+#Mengambil Nilai / indexing
+"""
+print('elemen ke 1 dari a adalah',a[0])
+print('elemen ke 7 dari a adalah',a[6])
+print('elemen terakhir dari a adalah',a[-1])]
+"""
+
+#slicing
+"""
+print('elemen dari 1-6 adalah ', a[0:7:1]) #start:end:steps
+"""
+
+#Iterasi
+"""for i in a:
+    print('value ke ', '=' , i)"""
--- indexing slicing dan lterasi pada numpy.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-arr_1d = np.array([10, 20, 30, 40, 50])
-print("Iterasi pada array 1D:")
-for elemen in arr_1d:
-    print(elemen)
